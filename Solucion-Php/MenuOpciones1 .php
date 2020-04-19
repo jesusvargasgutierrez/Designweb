661e53fc65f529ef8744173a5254c42ebf14c13f
jesusvargasgutierrez <jesvargtz_infc@hotmail.com>
2020-04-19 17:16:23 -0600

    back up structure database

---
+++ Solucion-Php/MenuOpciones1 .php
@@ -0,0 +1,82 @@
+<html>
+<head>
+    <title></title>
+    <meta charset="UTF-8">
+    
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    
+    <script defer src="https://use.fontawesome.com/releases/v5.0.13/js/solid.js" integrity="sha384-tzzSw1/Vo+0N5UhStP3bvwWPq+uvzCMfrN1fEFe+xBmv1C/AtVX5K0uZtmcHitFZ" crossorigin="anonymous"></script>
+    
+    <script defer src="https://use.fontawesome.com/releases/v5.0.13/js/fontawesome.js" integrity="sha384-6OIrr52G08NpOFSZdxxz1xdNSndlD4vdcf/q2myIUVO0VsqaGHJsB0RaBE01VTOY" crossorigin="anonymous"></script>
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js" integrity="sha384-cs/chFZiN24E4KMATLdqdvsezGxaGsi4hLGOzlXwp5UZB1LY//20VyM2taTB4QvJ" crossorigin="anonymous"></script>
+
+    <script src="https://code.jquery.com/jquery-3.4.1.min.js" type="text/javascript"></script>
+    <style type="text/css">
+        .navegacion ul{
+            list-style: none;
+        }
+
+        .submenu{
+            visibility: hidden;
+        }
+
+        .menu li:hover .submenu{
+            visibility: visible;
+        }
+        
+    </style>
+</head>
+<body>
+    <section class="mt-2">
+        <nav class="navbar navbar-expand-md navbar-light">
+            <div class="container">
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggler" data-toggle="collapse"
+                    data-target="#navegacion-fm" aria-controls="menu-principal" aria-expanded="false"
+                    aria-label="Ver mas">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                </div>
+                <div class="collapse navbar-collapse ml-auto" id="navegacion-fm">
+                    <ul class="nav navbar-nav">
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Inicio</a>
+                        </li>
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                Servicios
+                            </a>
+                            <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                                <a class="dropdown-item" href="#">consectetur adipisicing elit</a>
+                                <a class="dropdown-item" href="#">sed do eiusmod</a>
+                                <a class="dropdown-item" href="#">Ut enim ad minim veniam</a>
+                            </div>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Acerca de nosotros</a>
+                        </li>
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                           Productos
+                        </a>
+                            <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                                <a class="dropdown-item" href="#">Lorem ipsum dolor</a>
+                                <a class="dropdown-item" href="#">sed do eiusmod</a>
+                                <a class="dropdown-item" href="#">Ut enim ad minim veniam</a>
+                            </div>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Contactanos</a>
+                        </li>
+                    </ul>
+                </div>  
+            </div>
+        </nav>
+    </section>
+
+    <script src="style/bootstrap/js/bootstrap.min.js"></script>
+</body>
+</html>
