22d9be493dab00d31190234a44984d271df3a76c
jesusvargasgutierrez <jesvargtz_infc@hotmail.com>
2020-05-02 15:43:30 -0600

    it was realiced more file moved

---
+++ Solucion2/index.php
@@ -0,0 +1,156 @@
+<html>
+<head>
+    <title></title>
+    <meta charset="UTF-8">
+    
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    
+    <link href="style/style.css" rel="stylesheet">
+    
+    <script defer src="https://use.fontawesome.com/releases/v5.0.13/js/solid.js" integrity="sha384-tzzSw1/Vo+0N5UhStP3bvwWPq+uvzCMfrN1fEFe+xBmv1C/AtVX5K0uZtmcHitFZ" crossorigin="anonymous"></script>
+    
+    <script defer src="https://use.fontawesome.com/releases/v5.0.13/js/fontawesome.js" integrity="sha384-6OIrr52G08NpOFSZdxxz1xdNSndlD4vdcf/q2myIUVO0VsqaGHJsB0RaBE01VTOY" crossorigin="anonymous"></script>
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js" integrity="sha384-cs/chFZiN24E4KMATLdqdvsezGxaGsi4hLGOzlXwp5UZB1LY//20VyM2taTB4QvJ" crossorigin="anonymous"></script>
+
+    <script src="https://code.jquery.com/jquery-3.4.1.min.js" type="text/javascript"></script>
+
+    <link rel="stylesheet" href="style/fontawesome/css/fontawesome.css" rel="stylesheet">
+
+    <link rel="stylesheet" href="style/bootstrap/css/bootstrap.min.css">
+
+    <link href="style/fontawesome/css/brands.css" rel="stylesheet">
+
+    <link href="style/fontawesome/css/solid.css" rel="stylesheet">
+    
+    <style>
+        .seccion-principal{
+            background: #373B44;  /* fallback for old browsers */
+            background: -webkit-linear-gradient(to right, #4286f4, #373B44);  /* Chrome 10-25, Safari 5.1-6 */
+            background: linear-gradient(to right, #4286f4, #373B44);
+            background-size: cover;
+            width: 100%;
+            background-repeat: no-repeat;
+            padding: 20% 0px;
+        }
+
+        .Page-footer{
+            background: #4b6cb7;  /* fallback for old browsers */
+            background: -webkit-linear-gradient(to right, #182848, #4b6cb7);  /* Chrome 10-25, Safari 5.1-6 */
+            background: linear-gradient(to right, #182848, #4b6cb7);
+        }
+
+        .page-quien
+        {
+            background: #BBD2C5;  /* fallback for old browsers */
+            background: -webkit-linear-gradient(to right, #292E49, #536976, #BBD2C5);  /* Chrome 10-25, Safari 5.1-6 */
+            background: linear-gradient(to right, #292E49, #536976, #BBD2C5); 
+        }
+    </style>
+</head>
+<body>
+    <nav class="page-header navbar navbar-light bg-light navbar-expand-md">
+        <a class="navbar-brand" href="#">
+            <span class="h4">
+                Mi portafolio
+            </span>
+        </a>
+
+        <button type="button" class="navbar-toggler" data-toggle="collapse"
+        data-target="#menu-principal" aria-controls="menu-principal" aria-expanded="false"
+        aria-label="Ver mas">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="menu-principal">
+            <ul class="navbar-nav ml-auto">
+                <li class="nav-item"><a class="text-body nav-link" href="https://github.com/jesusvargasgutierrez/">Gihub&nbsp;<i class="fab fa-github"></i></a></li>
+                <li class="nav-item"><a class="text-body nav-link" href="https://www.linkedin.com/in/jesus-vargas-gutierrez-848b41187">Linkedin&nbsp;<i class="fab fa-linkedin"></i></a></li>
+            </ul>
+        </div>
+    </nav>
+
+    <div class="container">
+        <div class="row">
+            <div id="accordion" class="col-lg-4 col-md-12">
+                <div class="list-group" id="list-tab" role="tablist">
+                    <a class="list-group-item list-group-item-action anclalist" id="list-home-list" data-parent="#accordion" data-toggle="collapse" href="#list-home" role="tab" aria-controls="home">Programacion</a>
+                    <div class="" id="list-home">
+                        <ul class="list-group-item-text">
+                          <li>
+                            <button type="button" value="jsdiv" class="btn accion1">
+                              Javascript
+                            </button>
+                           </li>
+                          <li>
+                            <button type="button" value="phpdv" class="btn accion1">
+                              PHP
+                            </button>
+                           </li>
+                          <li>
+                            <button type="button" value="netdv" class="btn accion1">
+                              ASP.NET y Webservice
+                            </button>
+                           </li>
+                        </ul>
+                      </div>
+                    <a class="list-group-item list-group-item-action" id="list-profile-list" data-parent="#accordion" data-toggle="collapse" href="#list-profile" role="tab" aria-controls="profile">Bases de datos</a>
+                    <div class="" id="list-profile">
+                        <ul class="list-group-item-text">
+                          <li>
+                            <button type="button" value="sqldv" class="btn accion1">
+                             SQL Server
+                            </button>
+                          </li>
+                          <li>
+                            <button type="button" value="msdv" class="btn accion1">
+                              MySQL Server
+                            </button>
+                           </li>
+                        </ul>
+                    </div>
+                    <a class="list-group-item list-group-item-action" id="list-messages-list" data-parent="#accordion" data-toggle="collapse" href="#list-messages" role="tab" aria-controls="messages">Paginas Ejemplo</a>
+                    <div class="" id="list-messages">
+                        <ul class="list-group-item-text">
+                          <li><a href="https://coders-web.000webhostapp.com/">Ejemplo 1</a></li>
+                          <li><a href="https://coders-web.000webhostapp.com/Templates/Page1.html">Ejemplo 2</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-8 col-md-12" id="dvInformation">
+                <p id="jsdiv" class="text-justify d-none">
+                    Utilizado en menus, y acciones de controles combinando jquery
+                    <a href="Templates/JSExamples.html">ejemplos</a>
+                </p>
+                <p id="phpdv" class="text-justify d-none">
+                    <a href="Templates/PHPExamples.html">Ejemplos</a> con el lenguaje PHP sin utilizar algun framework de por medio y utilizando la POO
+                </p> 
+                <p id="netdv" class="text-justify d-none">
+                    <strong>ASP .NET</strong> es el entorno de trabajo diario si es necesario puedo crear una interfaz o bien a&ntilde;adir nuevas funciones
+                    utilizando el framework de dise&ntilde;o de boostrap
+                    web API para separar el front-end del backend algunos de ellos son para el envio de correos,
+                    importacion de informacion etc. <a href="Templates/ASPexamples.html">ejemplos</a>
+                </p>
+                <p id="sqldv" class="text-justify d-none">
+                    El lenguaje de consultas SQL server usado diariamente
+                    es posible crear desde simples procedimientos para el CRUD, 
+                    cursores o funciones dependiendo del requerimiento
+                    <a href="Templates/SQLServer.html">ejemplos</a>
+                </p>
+                <p id="msdv" class="text-justify d-none">
+                    Motor de base de datos utilizado en conjunto con PHP 
+                    en un entorno de linux, mas concretamente Ubuntu generalmente
+                    los procesos estan realizadas mediante rutinas utilizando la herramienta de Phpmyadmin 
+                </p>
+            </div>
+        </div>
+    </div>
+
+    <script src="script/portafolio.js"></script>
+    <script src="script/script.js"></script>
+    <script src="style/bootstrap/js/bootstrap.min.js"></script>
+</body>
+</html>
