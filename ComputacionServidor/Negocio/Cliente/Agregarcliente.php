c7ff5a6acec1a66d3a82837caf29f6a9c97226a7
jesusvargasgutierrez <jesvargtz_infc@hotmail.com>
2020-06-23 19:58:47 -0600

    - more examples for the test.

--- ComputacionServidor/Negocio/Cliente/Agregarcliente.php
+++ ComputacionServidor/Negocio/Cliente/Agregarcliente.php
@@ -61,10 +61,84 @@
         }
 
         /**
-         * Evaluamos la fecha y la convertimos a un formato de aaaa/mm/dd
+         * enviamos un mensaje en HTML para indicar que usaremos while
          */
-        //echo vprintf("%04d-%02d-%02d", explode('/', $_POST['DateBird']))."<br/>";
+        print("<br/> <br/><strong>Ejemplo usando while </strong> <br/>");
+
+        //creamos dos arreglos uno que contenga alumnos y el otro calificaciones
+        $Alumnos=array('AlumnoI','AlumnoII','AlumnoIII');
+        $Calificaciones=array('100','90','95');
+
+        //comenzamos la creacion de la tabla HTML
+        $tablaCalificaciones = "<table><thead><th>Alumno</th><th>Calificacion</th></thead> <tbody>";
+
+        //declaramos un indice
+        $indice = 0;
+
+        /**
+         * iniciamos el while, la diferencia es que primero convertiremos cada item
+         * del arreglo alumnos en una lista para ello es el lado derecho de la igualdad each ($Alumnos)
+         * el lado izquierdo nos proporciona la manipulacion por propiedad valor
+         */
+        while (list ($key, $val) = each ($Alumnos) )
+        {
+          //la primera columna contendra el alumno son solo 3 iteraciones
+          $tablaCalificaciones = $tablaCalificaciones . "<tr> <td>" . $val ."</td>";
+
+          /**
+           * aprovechando que el arreglo alumno y el arreglo calificaciones tienen la
+           * misma dimension podemos hacer uso de la variable indice al iniciar en 0
+           * e incrementara cada fin de recorrido 
+          */
+          $tablaCalificaciones = $tablaCalificaciones . "<td>" . $Calificaciones[$indice] ."</td> </tr>";
+
+          //aqui esta el incremento de la variable indice cada iteracion se incrementara 1
+          $indice = $indice + 1;
+        }//fin del while
+
+        /**
+         * concatenamos la tabla que se construyo en el while para añadirle el fin
+         * de la tabla
+         */
+        $tablaCalificaciones = $tablaCalificaciones . "</tbody></table>";
+
+        //desplegamos la tabla en pantalla con print
+        print($tablaCalificaciones);
+
+        /**
+         * enviamos un mensaje en HTML para indicar que usaremos do while
+         */
+        print("<br/> <br/><strong>Ejemplo usando do while </strong> <br/>");
+
+        //creamos un arreglo de 5 numeros
+        $numeros = array(100,50,5,20,78);
+
+        //declaramos un indice llamado j y se inicializa en 0
+        $j = 0;
+
+        /**
+         * inicia el do while, una particularidad importante del
+         * do while es que el recorrido lo hace al menos una vez, es decir,
+         * la condicion de si el residuo del numero del arreglo en igual a 0
+         * se evaluara al final, es por esa razon que el numero 5 le concatena
+         * el enunciado que es par pero obviamente no lo es, ya que al llegar
+         * a la condicion termina el bucle y ya no evalua los 2 numeros restantes
+         * del arreglo
+         */
+        do {
+          
+          //se pasa el valor del arreglo segun el valor del indice j
+          $numero_ = $numeros[$j];
+
+          //se concatena un enunciado y el numero
+          echo "<br/> El numero: ".$numero_ . " es par";
+
+          //se incrementa el indice j si la evaluacion se sigue cumpliendo
+          $j = $j+1;
+
+        } while (($numero_ % 2) == 0);//realiza la evaluacion de si el residuo del numero al dividirlo entre 2 es 0
     }else {
+      //si la variable no existe mostrara este mensaje
      echo "no existe";
     }
 ?>
