5925f1aa2b404501dd1e4a805a9766cbd7152c06
jesusvargasgutierrez <jesvargtz_infc@hotmail.com>
2020-08-09 20:09:08 -0600

    added more interfaces.

---
+++ ComputacionCliente/Negocio/Ejemplos/Codigo.js
@@ -0,0 +1,38 @@
+$(document).ready(function() {
+    $(".accion1").click(function() {
+        var Direccion = "../Negocio/Ejemplos/"+$(this).attr('value');
+        var Vista = $(this).data('id');
+
+        ObtenerCodigo(Direccion,Vista);
+
+    });
+});   
+
+function Showviews(Mostrarclase){
+    var countchild = $("#dvInformation").children().length;
+
+        $("."+Mostrarclase).addClass("d-block");
+
+        $("."+Mostrarclase).removeClass("d-none");
+
+        for (var i = 0; i < countchild; i++) {
+            var claseocultar = $("#dvInformation").children()[i].className;
+
+            if (claseocultar != Mostrarclase) {
+                $("."+claseocultar).addClass("d-none");
+            }
+        }
+}
+
+function ObtenerCodigo(url,vista)
+{
+        /**
+         * realizamos una peticion get 
+         * para obtener la estructura
+         */
+        $.get(url, function(html){
+            $("#dvInformation").html(html);
+
+            Showviews(vista);
+        });
+}
