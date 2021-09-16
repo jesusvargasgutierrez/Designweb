315b786befb5cd08e00a66a763e821cb25e4af4b
jesusvargasgutierrez <jesvargtz_infc@hotmail.com>
2021-09-16 23:12:36 +0200

    backup

--- digitalperson/app.js
+++ digitalperson/app.js
@@ -1,2 +1,111 @@
-import FingerprintJS from '@fingerprintjs/fingerprintjs';
-const esc = require('es6-shim');
+var test = null;
+var myVal = ""; // Drop down selected value of reader 
+var currentFormat = Fingerprint.SampleFormat.PngImage;
+
+var FingerprintSdkTest = (function () {
+    function FingerprintSdkTest() {
+        var _instance = this;
+        this.operationToRestart = null;
+        this.acquisitionStarted = false;
+        this.sdk = new Fingerprint.WebApi;
+        this.sdk.onDeviceConnected = function (e) {
+            // Detects if the deveice is connected for which acquisition started
+            ////showMessage(error.message);("Scan your finger");
+        };
+        this.sdk.onDeviceDisconnected = function (e) {
+            // Detects if device gets disconnected - provides deviceUid of disconnected device
+            //showMessage("Device disconnected");
+        };
+        this.sdk.onCommunicationFailed = function (e) {
+            // Detects if there is a failure in communicating with U.R.U web SDK
+            //showMessage("Communinication Failed")
+        };
+        this.sdk.onSamplesAcquired = function (s) {
+            // Sample acquired event triggers this function
+                sampleAcquired(s);
+        };
+        this.sdk.onQualityReported = function (e) {
+            // Quality of sample aquired - Function triggered on every sample acquired
+                //document.getElementById("qualityInputBox").value = Fingerprint.QualityCode[(e.quality)];
+        }
+
+    }
+
+    FingerprintSdkTest.prototype.startCapture = function () {
+        if (this.acquisitionStarted) // Monitoring if already started capturing
+            return;
+        var _instance = this;
+        //showMessage("");
+        this.operationToRestart = this.startCapture;
+        this.sdk.startAcquisition(currentFormat, myVal).then(function () {
+            _instance.acquisitionStarted = true;
+
+            //Disabling start once started
+            //disableEnableStartStop();
+
+        }, function (error) {
+            ////showMessage(error.message);
+        });
+    };
+    FingerprintSdkTest.prototype.stopCapture = function () {
+        if (!this.acquisitionStarted) //Monitor if already stopped capturing
+            return;
+        var _instance = this;
+        //showMessage("");
+        this.sdk.stopAcquisition().then(function () {
+            _instance.acquisitionStarted = false;
+
+            //Disabling stop once stoped
+            //disableEnableStartStop();
+
+        }, function (error) {
+            //showMessage(error.message);
+        });
+    };
+
+    FingerprintSdkTest.prototype.getInfo = function () {
+        var _instance = this;
+        return this.sdk.enumerateDevices();
+    };
+
+    FingerprintSdkTest.prototype.getDeviceInfoWithID = function (uid) {
+        var _instance = this;
+        return  this.sdk.getDeviceInfo(uid);
+    };
+
+    
+    return FingerprintSdkTest;
+})();
+
+window.onload = function () {
+    localStorage.clear();
+    //localStorage.setItem("raw", "");
+    test = new FingerprintSdkTest();
+};
+
+function onStart() {
+    test.startCapture();
+}
+
+/*$("#start").on("click",function(){
+    test.startCapture();
+
+    //downloadURI("data:application/octet-stream;base64,"+localStorage.getItem("raw"), "rawImage.raw", "application/octet-stream");
+});*/
+
+function sampleAcquired(s){   
+    localStorage.setItem("raw", "");
+    var samples = JSON.parse(s.samples);
+    console.log(samples);
+    var sampleData = Fingerprint.b64UrlTo64(samples[0].Data);
+    var decodedData = JSON.parse(Fingerprint.b64UrlToUtf8(sampleData));
+    localStorage.setItem("raw", Fingerprint.b64UrlTo64(decodedData.Data));
+}
+
+function downloadURI(uri, name, dataURIType) {
+
+}
+
+$("#save").on("click",function(){
+
+});
--- digitalperson/index.php
+++ digitalperson/index.php
@@ -1,42 +1,54 @@
-<?php
-    header('Access-Control-Allow-Origin: http://localhost:80');
-?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>FingerPrint</title>
+    <link href='https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css' rel='stylesheet'>
+    <link rel="stylesheet" href="style.css" type="text/css" />
+    <script type='text/javascript' src='https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js'></script>
 </head>
 <body>
-    hello world    
+    <div class="container mt-3">
+        <div class="row">
+            <div class="col">
+                <div id="imagediv"></div>
+            </div>
+            <div class="col-6">
+                <div id="status"></div>
+                <button class="btn btn-primary" id="start" onclick="Javascript:onStart()">start</button>
+                <button class="btn btn-success" id="save">save</button>
+            </div>
+        </div>
+    </div>     
 </body>
 <script src="es6-shim.js"></script>
 <script src="websdk.client.bundle.min.js"></script>
 <script src="fingerprint.sdk.min.js"></script>
+<script src="app.js"></script>
 <script>
     //https://127.0.0.1:9001/connect
-    this.sdk = new Fingerprint.WebApi;
-    var FingerprintSdkTest = (function () {
-    function FingerprintSdkTest() {
-        this.sdk = new Fingerprint.WebApi;
-    }
-    FingerprintSdkTest.prototype.getDeviceList = function () {
-            return this.sdk.enumerateDevices();
-        };
-        return FingerprintSdkTest;
-    })();
-    window.onload = function () {
-        test = new FingerprintSdkTest();
-        var allReaders = test.getDeviceList();    
-        allReaders.then(function (sucessObj) {
-            for (i=0;i<sucessObj.length;i++){
-                console.log(sucessObj[i]);
-            }
-        }, function (error){
-            console.log(error.message);
-        });
-    }
+    // this.sdk = new Fingerprint.WebApi;
+    // var FingerprintSdkTest = (function () {
+    // function FingerprintSdkTest() {
+    //     this.sdk = new Fingerprint.WebApi;
+    // }
+    // FingerprintSdkTest.prototype.getDeviceList = function () {
+    //         return this.sdk.enumerateDevices();
+    //     };
+    //     return FingerprintSdkTest;
+    // })();
+    // window.onload = function () {
+    //     test = new FingerprintSdkTest();
+    //     var allReaders = test.getDeviceList();    
+    //     allReaders.then(function (sucessObj) {
+    //         for (i=0;i<sucessObj.length;i++){
+    //             console.log(sucessObj[i]);
+    //         }
+    //     }, function (error){
+    //         console.log(error.message);
+    //     });
+    // }
 </script>
 </html>
