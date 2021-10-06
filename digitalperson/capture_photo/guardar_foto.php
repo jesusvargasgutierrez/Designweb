f7960e08783b489c59ca1d68e758f7d39870835d
jesusvargasgutierrez <jesvargtz_infc@hotmail.com>
2021-10-06 23:18:25 +0200

    cambios

--- digitalperson/capture_photo/guardar_foto.php
+++ digitalperson/capture_photo/guardar_foto.php
@@ -1,4 +1,26 @@
 <?php
-exit(array_keys($_POST));
+if(isset($_POST['action']) && $_POST['action'] == 1){
+    unlink($_POST['capture']);
+    exit('imagen eliminada!');
+}else{
+    $imagenCodificada = file_get_contents("php://input"); //Obtener la imagen
+
+    if(strlen($imagenCodificada) <= 0) exit("No se recibió ninguna imagen");
+    //La imagen traerá al inicio data:image/png;base64, cosa que debemos remover
+    $imagenCodificadaLimpia = str_replace("data:image/png;base64,", "", urldecode($imagenCodificada));
+    
+    //Venía en base64 pero sólo la codificamos así para que viajara por la red, ahora la decodificamos y
+    //todo el contenido lo guardamos en un archivo
+    $imagenDecodificada = base64_decode($imagenCodificadaLimpia);
+    
+    //Calcular un nombre único
+    $nombreImagenGuardada = "foto_" . uniqid() . ".png";
+    
+    //Escribir el archivo
+    file_put_contents($nombreImagenGuardada, $imagenDecodificada);
+    
+    //Terminar y regresar el nombre de la foto
+    exit($nombreImagenGuardada);
+}
 
 ?>
