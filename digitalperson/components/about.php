02ca0973688e81525271308ac0c3287d40371c64
jesusvargasgutierrez <jesvargtz_infc@hotmail.com>
2021-10-08 19:40:01 -0600

    .

---
+++ digitalperson/components/about.php
@@ -0,0 +1,86 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href='https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css' rel='stylesheet'>
+    <link href='https://use.fontawesome.com/releases/v5.7.2/css/all.css' rel='stylesheet'>
+    <script type='text/javascript' src='https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js'></script>
+</head>
+<body>
+    <div class="container">        
+        <p class="text-justify">
+            <div class="h4">Informacion de aplicaci&oacute;n</div>
+            Se trata de una aplicacion de prueba para la comprobar el correcto funcionamiento del hardware desde una aplicacion
+            web <br><br>
+            Considere este sitio como un ambiente de pruebas, tenga total libertad para realizar las pruebas que considere necesarias
+            la mayoria de las funcionalidades, iconos y botones tendr&aacute;n una gran similitud en el ambiente de produccion real
+            pero adaptados al flujo de operacion diario.
+        </p>
+        <p class="text-justify">
+            <div class="h4">Iconos</div>
+            En ocasiones, los botones contaran con iconos e lugar de texto, le invitamos a visualizar la siguiente tabla con la finalidad 
+            de conocer el significado de los mismos:
+
+            <table class="table table-bordered text-center">
+                <tr>
+                    <th>Icono</th>
+                    <th>Descripcion</th>
+                </tr>
+                <tr>
+                    <td>
+                        <i style="font-size:15pt" class="fas fa-save"></i>
+                    </td>
+                    <td>
+                        Guarda o actualiz&aacute; la informacion del proceso en el que se encuentre
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <i style="font-size:15pt" class="far fa-trash-alt"></i>
+                    </td>
+                    <td>
+                        Eliminar&aacute; el registro o la informacion actual
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <i style="font-size:15pt" class="fas fa-download"></i>
+                    </td>
+                    <td>
+                        Dependiendo del proceso descarga la informacion, fotografia / archivo 
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <i style="font-size:15pt" class="fas fa-play"></i>
+                    </td>
+                    <td>
+                        Inicia cualquier proceso en segundo plano, recomendamos estar atento a las notificaciones
+                        despues de presionarlo.
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <i style="font-size:15pt" class="fas fa-camera"></i>
+                    </td>
+                    <td>
+                        Cumple dos acciones:
+
+                        <ol>
+                            <li>
+                                Iniciara la camara conectada de la computadora del usuario
+                            </li>
+                            <li>
+                                Tomara una fotografia   
+                            </li>
+                        </ol>
+                    </td>
+                </tr>
+            </table>
+        </p>
+    </div>
+</body>
+</html>
