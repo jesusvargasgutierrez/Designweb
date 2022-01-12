6103d2146ec13d6724224e71c980d33a1fc2120f
jesusvargasgutierrez <jesvargtz_infc@hotmail.com>
2022-01-12 08:11:15 -0700

    .

--- model/awsimages.php
+++
@@ -1,8 +0,0 @@
-<?php
-    use Illuminate\Database\Eloquent\Model;
-
-class awsimages extends Model{
-    protected $table = "img_aws_presimap";
-    protected $primaryKey = 'id_img_aws';
-    protected $guarded = [];
-}
