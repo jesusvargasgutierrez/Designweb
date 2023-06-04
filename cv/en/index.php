fe75f2519d32120cc1642ad7783ef17ee43106b4
jesusvargasgutierrez <jesvargtz_infc@hotmail.com>
2023-06-04 09:15:35 -0600

    se agrega idioma

---
+++ cv/en/index.php
@@ -0,0 +1,63 @@
+<!DOCTYPE HTML>
+<html>
+  <?php include('page/header.php'); ?>
+  <?php include('page/body.php'); ?>
+    <br />
+    <div class="h3">Profile</div>
+    <div class="text-center">
+      <table class="table table-response">
+        <tr>
+          <td>
+            <i class="fas fa-address-card"></i>
+            MISSI. Jesus Vargas
+          </td>
+          <td>
+            <i class="fas fa-user"></i>
+            29 años
+          </td>
+          <td>
+            <a class="text-dark" href="mailto:jesvargtz@gmail.com">
+              <i class="fas fa-at"></i>
+              jesvargtz@gmail.com
+            </a>
+          </td>
+        </tr>
+        <tr>
+          <td>
+            <i class="fas fa-phone"></i>
+            +52 639-501-31-32
+          </td>
+          <td>
+            <a class="text-dark" href="https://www.linkedin.com/in/jesus-vargas-gutierrez-848b41187">
+              <i class="fab fa-linkedin-in"></i>
+              Linkedln
+            </a>
+          </td>
+          <td>
+            <a class="text-dark" href="https://github.com/jesusvargasgutierrez">
+              <i class="fab fa-github"></i>
+              Github
+            </a>
+          </td>
+          <td>&nbsp;</td>
+        </tr>
+      </table>
+    </div>
+    <p class="mt-5 text-justify">
+      Computer system enginier since 2016, with a big like by gadgets, videogames, technology, developer web, developer mobile, security and electronic mecanich
+      Developer own projects for warehouses, process logistic and fintech
+      <br /><br />
+
+      Collaborator develop external for to learn various methodologies and program languages <br />
+
+      Apassionate free software, security and server administration into cloud with Microsoft azure y Amazon Web Services.<br />
+      <br /> 
+      Actually work as developer software with methology SCRUM, develop webservices, eccomerce and credit calculates, to use program languajes server side with 
+      PHP, C#, Perl, Visual Basic, using frameworks with laravel and React Js.
+      <br /><br />
+      Using engine database relationals as MySql Server, SQL Server and non relational as MongoDB.
+      Building local enviroment develop starting docker image, or using frameworks as laragon, o XAMP Server, WAMP Server.
+    </p>
+
+  <?php include('page/footer.php'); ?>
+</html>
--- cv/en/page/body.php
+++ cv/en/page/body.php
@@ -4,14 +4,14 @@
          <nav class="header navbar navbar-expand-lg navbar-light bg-light body-pd" id="header">
             <div class="header_toggle d-none"> <i class='bx bx-menu' id="header-toggle"></i> </div>
             <a class="navbar-brand" style="font-size: 18pt; text-transform: capitalize !important;" href="{{ route('home') }}">
-               Curriculum
+               Resume
             </a> 
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
                <span class="navbar-toggler-icon"></span>
             </button>
-            <a href="https://coders-web.online/cv/en/" class="text-dark">            
+            <a href="https://coders-web.online/cv/es/" class="text-dark">            
                <i class="fas fa-globe"></i>
-               English
+               Spanish
             </a>
          </nav>
          <div class="l-navbar show" id="nav-bar">
@@ -19,35 +19,35 @@
                <div>
                   <a href="#" class="nav_logo"> 
                      <i class='bx bx-layer nav_logo-icon'></i> 
-                     <span class="nav_logo-name">Curriculum</span> 
+                     <span class="nav_logo-name">Resume</span> 
                   </a>
                   <div class="nav_list"> 
                     <li>
                        <a href="#{{ $module->description }}" data-toggle="collapse" aria-expanded="true" class="nav_link_new"> 
                           <i class='bx bx-grid-alt nav_icon'></i> 
                           <span class="nav_name">
-                             Secci&oacute;n
+                             Section
                           </span> 
                        </a>
                        <ul class="collapse list-unstyled show" id="{{ $module->description }}">
                           <li>
                              <a href="index.php" class="item-list connection-item" style="color:white;">
-                                Informaci&oacute;n
+                                Information
                              </a>
                           </li>
                           <li>
                              <a href="skills.php" class="item-list connection-item" style="color:white;">
-                                Habilidades
+                                Skills
                              </a>
                           </li>
                           <li>
                              <a href="experience.php" class="item-list connection-item" style="color:white;">
-                                Experiencia profesional
+                                Professional experience
                              </a>
                           </li>
                            <li>
-                             <a href="CV-JesusVargas.pdf" class="item-list connection-item" style="color:white;">
-                                Descargar CV
+                             <a href="Resume-JesusVargas.pdf" class="item-list connection-item" style="color:white;">
+                                Download resume
                              </a>
                           </li>
                        </ul>
